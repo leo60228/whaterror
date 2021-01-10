1c053f8c926647038c92eb5dfecd66bf95253f08
leo60228 <leo@60228.dev>
2021-01-09 19:51:47 -0500

    even more smoke tests

--- whaterror/tests/smoke.rs
+++ whaterror/tests/smoke.rs
@@ -32,6 +32,31 @@ fn simple_handler() -> Option<()> {
     None
 }
 
+#[whaterror(|| panic!(PanicMarker))]
+fn option_closure_handler() -> Option<()> {
+    None
+}
+
+#[whaterror(|err| { assert_eq!(err, 123); panic!(PanicMarker); })]
+fn result_failing() -> Result<(), i32> {
+    Err(123)
+}
+
+#[whaterror(|_| ())]
+fn result_passing() -> Result<(), ()> {
+    Ok(())
+}
+
+#[whaterror(drop)]
+fn named_handler() -> Result<(), ()> {
+    Ok(())
+}
+
+#[whaterror(())]
+fn no_panic_marker() -> Option<()> {
+    None
+}
+
 macro_rules! group {
     (#[$attr:meta] mod $group:ident { $($name:ident : $test:ident $args:tt;)* }) => {
         mod $group {
@@ -61,10 +86,15 @@ groups! {
         void_with_handler: void_with_handler();
         impure_handler: impure_handler();
         simple_handler: ensure_panic_marker(simple_handler);
+        option_closure_handler: ensure_panic_marker(simple_handler);
+        result_failing: ensure_panic_marker(simple_handler);
+        result_passing: result_passing();
+        named_handler: named_handler();
     }
 
     #[should_panic]
     mod should_fail {
         failing_option: failing_option();
+        no_panic_marker: ensure_panic_marker(no_panic_marker);
     }
 }
